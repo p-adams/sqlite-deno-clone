5d498d3d4180c70553a3c99a5377b8154ecd4f80
p-adams <ghusiyy@gmail.com>
2021-07-12 16:07:37 -0700

    get user input via prompt function

--- db.ts
+++ db.ts
@@ -1,1 +1,10 @@
-function main() {}
+function main() {
+  while (true) {
+    const input = prompt();
+    if (input === ".exit") {
+      return 0;
+    }
+    console.log(`Unrecognized command ${input}`);
+  }
+}
+main();
